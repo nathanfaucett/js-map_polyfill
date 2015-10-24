a484cc24c8e8df5a210511949527cee45b372c77
nathanfaucett <nathanfaucett@gmail.com>
2015-10-23 21:07:12 -0500

    updates

---
+++ test/index.js
@@ -0,0 +1,47 @@
+var tape = require("tape");
+
+
+var Map;
+
+
+global.Map = undefined;
+Map = require("..");
+
+
+tape("Map#get(key)", function(assert) {
+    var a = new Map();
+    a.set("key", "value");
+    assert.equal(a.get("key"), "value", "should return value at key, else returns undefined");
+    assert.equal(a.get("no"), undefined, "should return value at key, else returns undefined");
+    assert.end();
+});
+
+tape("Map#set(key, value)", function(assert) {
+    var a = new Map();
+    a.set("key", "value");
+    assert.equal(a.get("key"), "value", "should set value at key");
+    assert.end();
+});
+
+tape("Map#has(key)", function(assert) {
+    var a = new Map();
+    a.set("key", "value");
+    assert.equal(a.has("key"), true, "should return true if map contains key");
+    assert.end();
+});
+
+tape("Map#remove(key)", function(assert) {
+    var a = new Map();
+    a.set("key", "value");
+    a.remove("key");
+    assert.equal(a.has("key"), false, "should remove value stored at key");
+    assert.end();
+});
+
+tape("Map#clear()", function(assert) {
+    var a = new Map();
+    a.set("key", "value");
+    a.clear();
+    assert.equal(a.count(), 0, "should remove all keys from map");
+    assert.end();
+});
